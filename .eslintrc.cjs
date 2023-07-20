c874c55c74fc61658607db3926694937a9f2e179
Venilton Junior <venilton.junior.dev@gmail.com>
2023-07-19 23:36:11 -0300

    chore: add eslint

---
+++ .eslintrc.cjs
@@ -0,0 +1,13 @@
+/* eslint-env node */
+module.exports = {
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  root: true,
+  rules: {
+    "@typescript-eslint/consistent-type-imports": "off",
+    "@typescript-eslint/strict-boolean-expressions": "off",
+    "@typescript-eslint/method-signature-style": "off",
+    "@typescript-eslint/consistent-type-definitions": "off"
+  }
+}
